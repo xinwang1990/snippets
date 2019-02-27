65afc26d62dc8d63e4cbacbfd89c852ecb518fdc
xinwang1990 <xinwang1990@163.com>
2019-02-28 00:13:46 +0800

    Update family_of_crytal_planes.py

--- family_of_crytal_planes.py
+++ family_of_crytal_planes.py
@@ -1,9 +1,11 @@
 import itertools
 import pandas as pd
 
-df = pd.DataFrame(list(itertools.permutations([1,1,0],3))) #change the index you want
+df = pd.DataFrame(list(itertools.permutations([1,1,0],3))) # CHANGE THE MILLER INDEX YOU WANT
 df = pd.concat([df, df.mul([-1, 1, 1]), df.mul([1, -1, 1]), df.mul([1, 1, -1]), df.mul([-1, -1, 1]), df.mul([-1, 1, -1]),df.mul([1, -1, -1]),df.mul([-1, -1, -1])], ignore_index=True)
 df = df.drop_duplicates()
 df = df.reset_index(drop=True)
 
 print(df.to_string(index=False, header=False))
+
+# THIS IS ONLY VALID FOR CUBIC CRYSTALS
